da7ef29acc78d1f18f8796b7ddca0f27221ecd3a
semawit <semawitaraya@gmail.com>
2022-07-27 11:17:48 -0400

    update codes

--- create_tables.py
+++ create_tables.py
@@ -8,18 +8,19 @@ from sql_queries import create_table_queries, drop_table_queries
     - Returns the connection and cursor to sparkifydb
     """
 def create_database():
-    conn=psycopg2.connect("host=127.0.0.1 dbname=studentdb user=semawitaraya@gmail.com password=admin123")
+    conn=psycopg2.connect("host=127.0.0.1 dbname=studentdb user=semawit password=semawit")
     conn.set_session(autocommit=True)
     cur=conn.cursor()
 
 #create sparkify database with UTF8 encoding
+
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.excutes("CREAT DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
  # close connection to default database
     conn.close()
 
 #connect to sparkify database
-    conn=psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=semawitaraya@gmail.com password=admin123")
+    conn=psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=semawit password=semawit")
     cur=conn.cursor()
 
     return cur,conn
@@ -28,8 +29,8 @@ def drop_tables(cur,conn):
     """
     Drops each table using the queries in `drop_table_queries` list.
     """
-    for query in drop_table_queries:
-        cur.excecute(query)
+    for query in drop_table_queries:1``
+        cur.execute(query)
         conn.commit()
 
 def create_tables(cur,conn):
@@ -39,6 +40,7 @@ def create_tables(cur,conn):
      for query in create_table_queries:
         cur.execute(query)
         conn.commit()
+        print("table created sucessfully")
 def main():
     """
     - Drops (if exists) and Creates the sparkify database. 
